23b6143be4eed668a73a8a59d80737014bf0a900
Yirong Qin <yq4103@mun.ca>
2022-05-26 18:28:08 -0400

    fix backend

--- src/Components/WeatherSearch.js
+++ src/Components/WeatherSearch.js
@@ -1,24 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Body from "./Body";
-import { createBrowserHistory } from "history";
 
 export function WeatherSearch() {
-  const history = createBrowserHistory();
   const [weatherForm, setWeatherForm] = useState("");
   const [weatherType, setWeatherType] = useState("");
-
-  useEffect(() => {
-    if (weatherType === "daily") {
-      history.push("hourly");
-    }
-    if (weatherType === "hourly") {
-      history.push("daily");
-    }
-    if (weatherType === "") {
-      history.push("");
-    }
-  }, [weatherType, history]);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { WeatherSearch } from "./Components/WeatherSearch";
-import { BrowserRouter } from "react-router-dom";
 
 import "./Styles/styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <WeatherSearch />
-    </BrowserRouter>
+    <WeatherSearch />
   </React.StrictMode>
 );
