c1ecfa7f4e4dafa0d0e2b75b0cf51855db646ed9
Yirong Qin <yq4103@mun.ca>
2022-05-26 18:10:32 -0400

    fix backend

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.use(express.json());
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.json(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
